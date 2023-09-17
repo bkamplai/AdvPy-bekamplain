b7e53c7c0502fadd8e9020d72de748f117657596
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-09-16 18:23:07 -0600

    Kattis - Aaah

--- kattis/Aaah!/main.py
+++ kattis/Aaah!/main.py
@@ -1,22 +1,24 @@
 # Difficult: 2.0
 
-def count_a(s):
-    count = 0
+def count_a(s: str) -> int:
+    count: int = 0
     while count < len(s) and s[count] == "a":
         count += 1
     return count
 
-def compare_count_a(person1, person2):
-    jon_count = count_a(person1)
-    doctor_count = count_a(person2)
+
+def compare_count_a(person1: str, person2: str) -> str:
+    jon_count: int = count_a(person1)
+    doctor_count: int = count_a(person2)
 
     if doctor_count > jon_count:
         return "no"
     else:
         return "go"
-    
+
+
 if __name__ == "__main__":
-    jon = input()
-    doctor = input()
-    result = compare_count_a(jon, doctor)
+    jon: str = input()
+    doctor: str = input()
+    result: str = compare_count_a(jon, doctor)
     print(result)
--- kattis/Aaah!/test_unit_main.py
+++ kattis/Aaah!/test_unit_main.py
@@ -2,27 +2,27 @@ import unittest
 from main import count_a, compare_count_a
 
 class TestMainFunctions(unittest.TestCase):
-    def test_both_empty_strings(self):
+    def test_both_empty_strings(self) -> None:
         result = compare_count_a("", "")
         self.assertEqual(result, "go")
 
-    def test_one_empty_string(self):
+    def test_one_empty_string(self) -> None:
         result = compare_count_a("aaahhh", "")
         self.assertEqual(result, "go")
 
-    def test_both_strings_with_no_a(self):
+    def test_both_strings_with_no_a(self) -> None:
         result = compare_count_a("hhh", "hhh")
         self.assertEqual(result, "go")
 
-    def test_jon_has_more_a(self):
+    def test_jon_has_more_a(self) -> None:
         result = compare_count_a("aaah", "aah")
         self.assertEqual(result, "go")
 
-    def test_doctor_has_more_a(self):
+    def test_doctor_has_more_a(self) -> None:
         result = compare_count_a("aah", "aaah")
         self.assertEqual(result, "no")
 
-    def test_equal_number_of_a(self):
+    def test_equal_number_of_a(self) -> None:
         result = compare_count_a("aaah", "aaah")
         self.assertEqual(result, "go")
 
