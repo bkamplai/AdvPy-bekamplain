ab82527d01540823493fb13aa97b159300503065
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-09-21 14:14:57 -0600

    Kattis: Simple Addition

--- kattis/Simple Addition/main.py
+++ kattis/Simple Addition/main.py
@@ -1,28 +1,38 @@
 # Difficulty: 2.0
+from typing import Union
 
-def is_positive(num1, num2):
+
+def is_positive(num1: int, num2: int) -> bool:
     if num1 > 0 and num2 > 0:
         return True
     else:
         return False
 
-def fits_domain(num1, num2):
+
+def fits_domain(num1: int, num2: int) -> bool:
     if num1 < 10**10_000 and num2 < 10 ** 10_000:
         return True
     else:
         return False
 
-def is_digit(num1, num2):
+
+def is_digit(num1: str, num2: str) -> bool:
     if num1.isdigit() and num2.isdigit():
         return True
     else:
         return False
-    
-def sum(num1, num2):
-    if is_positive(int(num1), int(num2)) and fits_domain(int(num1), int(num2))and is_digit(num1, num2):
-        return eval(num1) + eval(num2)
+
+
+def sum(num1: str, num2: str) -> Union[int, None]:
+    if (
+        is_positive(int(num1), int(num2))
+        and fits_domain(int(num1), int(num2))
+        and is_digit(num1, num2)
+    ):
+        return int(num1) + int(num2)
     else:
         return None
+
 
 if __name__ == "__main__":
     x = input()
@@ -30,4 +40,3 @@ if __name__ == "__main__":
 
     result = sum(x, y)
     print(result)
-    
--- kattis/Simple Addition/test_unit_main.py
+++ kattis/Simple Addition/test_unit_main.py
@@ -1,33 +1,37 @@
 import unittest
+from hypothesis import given, strategies as st # type: ignore
 from main import is_positive, fits_domain, sum
 
-class TestMain(unittest.TestCase):
-    def test_is_positive(self):
-        self.assertTrue(is_positive(5, 10))
-        self.assertTrue(is_positive(0.1, 0.01))
-        self.assertFalse(is_positive(-5, -10))
-        self.assertFalse(is_positive(-0.1, -0.01))
-        self.assertFalse(is_positive(5, -10))
-        self.assertFalse(is_positive(-5, 10))
-    
-    def test_fits_domain(self):
-        self.assertTrue(fits_domain(9999999999, 9999999999))
-        self.assertFalse(fits_domain(10**10000, 10**10000))
 
-    def test_sum_valid_input(self):
+class TestMain(unittest.TestCase):
+    @given(st.integers(min_value=1), st.integers(min_value=1)) # type: ignore
+    def test_is_positive(self, num1: int, num2: int) -> None:
+        self.assertTrue(is_positive(num1, num2))
+
+
+    @given(st.integers(max_value=10**10), st.integers(max_value=10**10)) # type: ignore
+    def test_fits_domain(self, num1: int, num2: int) -> None:
+        self.assertTrue(fits_domain(num1, num2))
+
+
+    def test_sum_valid_input(self) -> None:
         self.assertEqual(sum("5", "10"), 15)
-    
-    def test_sum_invalid_input_negative_numbers(self):
+
+
+    def test_sum_invalid_input_negative_numbers(self) -> None:
         self.assertIsNone(sum("-5", "10"))
         self.assertIsNone(sum("-5", "-10"))
-    
-    def test_sum_invalid_input_non_numeric(self):
+
+
+    def test_sum_invalid_input_non_numeric(self) -> None:
         with self.assertRaises(ValueError):
             sum("abc", "def")
-    
-    def test_sum_invalid_input_mixed_format(self):
+
+
+    def test_sum_invalid_input_mixed_format(self) -> None:
         with self.assertRaises(ValueError):
             sum("5", "xyz")
+
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
